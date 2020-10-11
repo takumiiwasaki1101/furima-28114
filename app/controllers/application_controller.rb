a28c77df691c11a5acf5a51c9a0bc2fa0de58e2c
takumiiwasaki1101 <lcmwanol@gmail.com>
2020-10-11 12:10:25 +0900

    Rubocopに基づく修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit
-    (:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_furigana, :first_name_furigana, :date_of_birth])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_furigana, :first_name_furigana, :date_of_birth])
   end
 end

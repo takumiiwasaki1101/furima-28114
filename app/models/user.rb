dec6273e037cd859be4b3f6eb561753c7c132c15
takumiiwasaki1101 <lcmwanol@gmail.com>
2020-10-11 09:26:13 +0900

    エラーメッセージの表示（２

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  # FULL_WIDTH_KANA_REGEX = /^([ァ-ン]|ー)+$/i.freeze
+  FULL_WIDTH_KANA_REGEX = /\A[ァ-ン]+\z/i.freeze
   FULL_WIDTH_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/i.freeze
 
   validates_format_of :encrypted_password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'        
@@ -15,11 +15,9 @@ class User < ApplicationRecord
   validates :first_name,           presence: true
     validates_format_of :first_name, with: FULL_WIDTH_REGEX, message: 'Full-width characters'
   validates :family_name_furigana, presence: true
-    # validates_format_of :family_name_furigana, with: FULL_WIDTH_KANA_REGEX, message: 'Full-width katakana characters'        
+    validates_format_of :family_name_furigana, with: FULL_WIDTH_KANA_REGEX, message: 'Full-width katakana characters'        
   validates :first_name_furigana,  presence: true
-    # validates_format_of :first_name_furigana, with: FULL_WIDTH_KANA_REGEX, message: 'Full-width katakana characters'        
+    validates_format_of :first_name_furigana, with: FULL_WIDTH_KANA_REGEX, message: 'Full-width katakana characters'        
   validates :date_of_birth,        presence: true
-
-  
 
 end

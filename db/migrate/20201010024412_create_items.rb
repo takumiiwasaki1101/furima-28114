ba1aae3cfcc258fe7f1d7ece42cd83e6ab58e237
takumiiwasaki1101 <lcmwanol@gmail.com>
2020-10-17 16:12:13 +0900

    activehashによるステータスデータの作成

--- db/migrate/20201010024412_create_items.rb
+++ db/migrate/20201010024412_create_items.rb
@@ -5,7 +5,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       # 商品一覧表示機能作成時のみitemモデルを一時的に簡略化
       t.text       :detail,          null:false
       t.integer    :category_id,     null:false
-      # t.integer    :status_id,       null:false
+      t.integer    :status_id,       null:false
       # t.integer    :cost_burden_id,  null:false
       # t.integer    :ship_from_id,    null:false
       # t.integer    :shipping_day_id, null:false

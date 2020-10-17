3c914aaae4a6bb49a192b528e03ec2627cd2dbb2
takumiiwasaki1101 <lcmwanol@gmail.com>
2020-10-17 13:34:12 +0900

    バリデーションにおける同様のオプションを一括化

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,23 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # 正規表現を変数設定         
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   FULL_WIDTH_KANA_REGEX = /\A[ァ-ン]+\z/i.freeze
   FULL_WIDTH_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/i.freeze
 
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
-  validates :nickname,             presence: true
-  validates :family_name,          presence: true
-  validates_format_of :family_name, with: FULL_WIDTH_REGEX, message: 'Full-width characters'
-  validates :first_name,           presence: true
-  validates_format_of :first_name, with: FULL_WIDTH_REGEX, message: 'Full-width characters'
-  validates :family_name_furigana, presence: true
-  validates_format_of :family_name_furigana, with: FULL_WIDTH_KANA_REGEX, message: 'Full-width katakana characters'
-  validates :first_name_furigana, presence: true
-  validates_format_of :first_name_furigana, with: FULL_WIDTH_KANA_REGEX, message: 'Full-width katakana characters'
-  validates :date_of_birth, presence: true
+  with_options presence: true do
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+    validates :nickname
+    validates :family_name
+    validates_format_of :family_name, with: FULL_WIDTH_REGEX, message: 'Full-width characters'
+    validates :first_name
+    validates_format_of :first_name, with: FULL_WIDTH_REGEX, message: 'Full-width characters'
+    validates :family_name_furigana
+    validates_format_of :family_name_furigana, with: FULL_WIDTH_KANA_REGEX, message: 'Full-width katakana characters'
+    validates :first_name_furigana 
+    validates_format_of :first_name_furigana, with: FULL_WIDTH_KANA_REGEX, message: 'Full-width katakana characters'
+    validates :date_of_birth
+  end
 end

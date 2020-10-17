da6a575ce1648e3d3d5797d4167ffbb8de4cfa93
takumiiwasaki1101 <lcmwanol@gmail.com>
2020-10-17 15:39:56 +0900

    未ログインユーザーをログイン画面へリダイレクト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,23 @@
 class ItemsController < ApplicationController
+  before_action :move_to_new_user_session_path, except: :index
+
   def index
     @items = Item.all.order('created_at DESC')
+  end
+
+  def new
+
+  end
+
+  def create
+    
+  end
+
+  private
+
+  def move_to_new_user_session_path
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end

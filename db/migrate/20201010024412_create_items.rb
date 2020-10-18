a3d40182a4261ce3f47d5851963ad0e74ca55a0f
takumiiwasaki1101 <lcmwanol@gmail.com>
2020-10-18 17:19:07 +0900

    itemsモデルからimageカラムを削除

--- db/migrate/20201010024412_create_items.rb
+++ db/migrate/20201010024412_create_items.rb
@@ -2,7 +2,6 @@ class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
       t.string     :name,            null:false
-      # 商品一覧表示機能作成時のみitemモデルを一時的に簡略化
       t.text       :detail,          null:false
       t.integer    :category_id,     null:false
       t.integer    :status_id,       null:false
@@ -10,8 +9,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer    :ship_from_id,    null:false
       t.integer    :shipping_day_id, null:false
       t.text       :image
-      t.integer    :price,           null:false
-      
+      t.integer    :price,           null:false      
       # t.references :user,            null:false, foreign_key:true      
       t.timestamps
     end

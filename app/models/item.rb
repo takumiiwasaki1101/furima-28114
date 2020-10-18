3616f7c381ca4e4208e9912d326256bc2e997524
takumiiwasaki1101 <lcmwanol@gmail.com>
2020-10-18 18:50:52 +0900

    バリデーションを記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,19 @@
 class Item < ApplicationRecord
+  # Association
   belongs_to :user
   has_one_attached :image
+
+  # Validation
+  with_options presence: true do
+    validates :name
+    validates :detail
+    validates :category_id, numericality: { other_than: 0}
+    validates :status_id, numericality: { other_than: 0}
+    validates :cost_burden_id, numericality: { other_than: 0}
+    validates :ship_from_id, numericality: { other_than: 0}
+    validates :shipping_day_id, numericality: { other_than: 0}
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
+  end}
+    validates :user_id
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,8 +19,9 @@ class User < ApplicationRecord
     validates :first_name_furigana 
     validates_format_of :first_name_furigana, with: FULL_WIDTH_KANA_REGEX, message: 'Full-width katakana characters'
     validates :date_of_birth
+  end
 
   # Association
   has_many :items
-  end
+
 end

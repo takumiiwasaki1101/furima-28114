03796ffadeb584152cbcb2d0101b2053b0223e48
takumiiwasaki1101 <lcmwanol@gmail.com>
2020-10-24 14:49:58 +0900

    単体テストコード（成功時

---
+++ spec/factories/items.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :item do
+    name { Faker::Name.name }
+    detail { Faker::Lorem.sentence }
+    category_id {Faker::Number.within(range: 1..10)}
+    status_id {Faker::Number.within(range: 1..6)}
+    cost_burden_id {Faker::Number.within(range: 1..2)}
+    ship_from_id {Faker::Number.within(range: 1..47)}
+    shipping_day_id {Faker::Number.within(range: 1..3)}
+    price {Faker::Number.within(range: 300..9999999)}
+    association :user
+  end
+end

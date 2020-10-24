b477e895638370ff8192135938bd381e01a6b831
takumiiwasaki1101 <lcmwanol@gmail.com>
2020-10-24 12:08:20 +0900

    validationの補記

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,16 +4,26 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   # Validation
-  with_options presence: true do
-    validates :image
-    validates :name
-    validates :detail
-    validates :category_id, numericality: { other_than: 0}
-    validates :status_id, numericality: { other_than: 0}
-    validates :cost_burden_id, numericality: { other_than: 0}
-    validates :ship_from_id, numericality: { other_than: 0}
-    validates :shipping_day_id, numericality: { other_than: 0}
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
-    validates :user_id
+    ## 入力必須に関するvalidation  
+    with_options presence: true do
+      validates :image
+      validates :name
+      validates :detail
+      validates :category_id, numericality: { other_than: 0, message: "Select
+        "}
+      validates :status_id, numericality: { other_than: 0, message: "Select
+        "}
+      validates :cost_burden_id, numericality: { other_than: 0, message: "Select
+        "}
+      validates :ship_from_id, numericality: { other_than: 0, message: "Select
+        "}
+      validates :shipping_day_id, numericality: { other_than: 0, message: "Select
+        "}
+      validates :price
+      validates :user_id
+    end
+    ## 価格範囲に関するvalidation
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range"}
+    ## 文字制限に関するvalidation
+    validates :price, format: {with: /\A[0-9]+\z/, message: "Half-width number"}
   end
-end

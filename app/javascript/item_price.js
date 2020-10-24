5d3a1c5572e9e93434c81609a1b6398e855294e3
takumiiwasaki1101 <lcmwanol@gmail.com>
2020-10-25 07:44:59 +0900

    手数料と利益情報を非同期通信により取得

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,6 +1,13 @@
 window.addEventListener('load', () => {
+//価格情報の取得
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
-    console.log("イベント発火");
-  })
+    const inputValue = priceInput.value;
+    // 販売手数料情報の取得
+    const addTaxDom = document.getElementById('add-tax-price');
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+    // 販売利益情報の取得
+    const profitDom = document.getElementById('profit');
+    profitDom.innerHTML = Math.floor(inputValue - addTaxDom.innerHTML);
+  });
 });

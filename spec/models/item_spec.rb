a816d0bb0af000bc307c0c4b32a958d896a3bc81
takumiiwasaki1101 <lcmwanol@gmail.com>
2020-10-24 19:39:45 +0900

    単体テストコード（失敗時

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -9,120 +9,82 @@ RSpec.describe Item, type: :model do
 
     context '新規登録成功時' do
       it 'image、name、detail、category_id、status_id、cost_burden_id、
-          ship_burden_id、ship_from_id、shipping_day_id、priceが存在すれば登録できること' do
+          ship_from_id、shipping_day_id、priceが存在すれば登録できること' do
       expect(@item).to be_valid
       end
+    end
+
+    context '新規登録失敗時' do
+      # 空では登録できないことを確認
+        it 'imageが空では登録できないこと' do
+          @item.image = nil
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Image can't be blank")
+        end
+
+        it 'nameが空では登録できないこと' do
+          @item.name = nil
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Name can't be blank")
+        end
+
+        it 'detailが空では登録できないこと' do
+          @item.detail = nil
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Detail can't be blank")
+        end
+
+        it 'category_idが空では登録できないこと' do
+          @item.category_id = nil
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Category can't be blank")
+        end
+
+        it 'status_idが空では登録できないこと' do
+          @item.status_id = nil
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Status can't be blank")
+        end
+
+        it 'ship_from_idが空では登録できないこと' do
+          @item.ship_from_id = nil
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Ship from can't be blank")
+        end
+
+        it 'shipping_day_idが空では登録できないこと' do
+          @item.shipping_day_id = nil
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Shipping day can't be blank")
+        end
+
+        it 'priceが空では登録できないこと' do
+          @item.price = nil
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Price can't be blank")
+        end
+      # 空では登録できないことを確認
+
+      # 範囲外の価格では登録できないことを確認
+        it 'priceが¥300~¥9,999,999の間でないと登録できないこと' do
+          @item.price = 200
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Price Out of setting range")
+        end
+      # 範囲外の価格では登録できないことを確認
+
+      # 販売価格は半角数字のみ保存可能であることを確認
+        it 'priceはが全角数字だと保存できないこと' do
+          @item.price = '３４００'
+          @item.valid?
+          expect(@item.errors.full_messages).to include('Price Half-width number')
+        end
+      # 販売価格は半角数字のみ保存可能であることを確認
     end
   end
 end
 
-#       it 'passwordが6文字以上であれば登録できること' do
-#         @user.password = 'T12345'
-#         @user.password_confirmation = 'T12345'
-#         expect(@user).to be_valid
-#       end
 
-#       it 'passwordが半角英数字混合であれば登録できること' do
-#         @user.password = 'T123456'
-#         @user.password_confirmation = 'T123456'
-#         @user.valid?
-#         expect(@user).to be_valid
-#       end
-#     end
-
-#     context '新規登録失敗時' do
-#       # 空では登録できないことを確認
-#       it 'nicknameが空では登録できないこと' do
-#         @user.nickname = nil
-#         @user.valid?
-#         expect(@user.errors.full_messages).to include("Nickname can't be blank")
-#       end
-
-#       it 'emailが空では登録できないこと' do
-#         @user.email = nil
-#         @user.valid?
-#         expect(@user.errors.full_messages).to include("Email can't be blank")
-#       end
-
-#       it 'passwordが空では登録できないこと' do
-#         @user.password = nil
-#         @user.valid?
-#         expect(@user.errors.full_messages).to include("Password can't be blank")
-#       end
-
-#       it 'family_nameが空では登録できないこと' do
-#         @user.family_name = nil
-#         @user.valid?
-#         expect(@user.errors.full_messages).to include("Family name can't be blank")
-#       end
-
-#       it 'first_nameが空では登録できないこと' do
-#         @user.first_name = nil
-#         @user.valid?
-#         expect(@user.errors.full_messages).to include("First name can't be blank")
-#       end
-
-#       it 'family_name_furiganaが空では登録できないこと' do
-#         @user.family_name_furigana = nil
-#         @user.valid?
-#         expect(@user.errors.full_messages).to include("Family name furigana can't be blank")
-#       end
-
-#       it 'first_name_furiganaが空では登録できないこと' do
-#         @user.first_name_furigana = nil
-#         @user.valid?
-#         expect(@user.errors.full_messages).to include("First name furigana can't be blank")
-#       end
-
-#       it 'date_of_birthが空では登録できないこと' do
-#         @user.date_of_birth = nil
-#         @user.valid?
-#         expect(@user.errors.full_messages).to include("Date of birth can't be blank")
-#       end
-#       # /空では登録できないことを確認
-
-#       # emailに@が存在しない場合登録できないことを確認
-#       it 'emailに@が存在しない場合登録できないこと' do
-#         @user.email = "hoge"
-#         @user.valid?
-#         expect(@user.errors.full_messages).to include('Email is invalid')
-#       end
-#       # /emailに@が存在しない場合登録できないことを確認
-
-#       # 重複したemailが存在する場合登録できないことを確認
-#       it '重複したemailが存在する場合登録できないこと' do
-#         @user.save
-#         another_user = FactoryBot.build(:user, email: @user.email)
-#         another_user.valid?
-#         expect(another_user.errors.full_messages).to include('Email has already been taken')
-#       end
-#       # /重複したemailが存在する場合登録できないことを確認
-      
-#       # パスワードの文字数制限を確認
-#       it 'passwordが5文字以下であれば登録できないこと' do
-#         @user.password = 'T1234'
-#         @user.password_confirmation = 'T1234'
-#         @user.valid?
-#         expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
-#       end
-#       # /パスワードの文字数制限を確認
-
-#       # パスワードの不一致の場合は登録できないことを確認
-#       it 'passwordとpassword_confirmationが不一致では登録できないこと' do
-#         @user.password = 'T12345'
-#         @user.password_confirmation = 'T123456'
-#         @user.valid?
-#         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
-#       end
-#       # パスワードの不一致の場合は登録できないことを確認
-
-#       # パスワードは半角英数字混合が必須であることを確認
-#       it 'passwordが半角数字のみであれば登録できないこと' do
-#         @user.password = '123456'
-#         @user.password_confirmation = '123456'
-#         @user.valid?
-#         expect(@user.errors.full_messages).to include('Password Include both letters and numbers')
-#       end
 
 #       it 'passwordが半角英語のみであれば登録できないこと' do
 #         @user.password = 'abcdef'

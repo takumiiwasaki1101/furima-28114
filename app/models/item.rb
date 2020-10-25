e4269dcea1ae52fa4a9ea8fa1a5e0b7777b6bc36
takumiiwasaki1101 <lcmwanol@gmail.com>
2020-10-25 11:09:43 +0900

    同一オプションをwith_optionsに修正(numericality

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,14 +16,23 @@ class Item < ApplicationRecord
       validates :image
       validates :name
       validates :detail
-      validates :category_id, numericality: { other_than: 0, message: "Select"}
-      validates :status_id, numericality: { other_than: 0, message: "Select"}
-      validates :cost_burden_id, numericality: { other_than: 0, message: "Select"}
-      validates :ship_from_id, numericality: { other_than: 0, message: "Select"}
-      validates :shipping_day_id, numericality: { other_than: 0, message: "Select"}
-      # validates :price
+      validates :category_id
+      validates :status_id
+      validates :cost_burden_id
+      validates :ship_from_id
+      validates :shipping_day_id
       validates :price, numericality: {with: /\A[0-9]+\z/, message: "Half-width number"}
     end
+
+    ## id0以外での入力を必須とするvalidation
+    with_options numericality: { other_than: 0, message: "Select"}  do
+      validates :category_id
+      validates :status_id
+      validates :cost_burden_id
+      validates :ship_from_id
+      validates :shipping_day_id
+    end
+
     ## 価格範囲に関するvalidation
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range"} 
 end

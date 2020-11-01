0988c067b7d8685b529b4a8841171e971b3d58bc
takumiiwasaki1101 <lcmwanol@gmail.com>
2020-11-01 12:30:21 +0900

    コントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,6 +23,20 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render action: 'edit'    # バリデーションに弾かれた時
+    end
+  end
+  
+
   private
 
   def item_params

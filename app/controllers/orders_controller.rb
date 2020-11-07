0de0cd3ef5ef85374e409cdbfec012d245a8005d
takumiiwasaki1101 <lcmwanol@gmail.com>
2020-11-07 14:24:59 +0900

    配送先の登録

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,8 @@ class OrdersController < ApplicationController
   def create
     @order = Order.new(user_id: current_user.id, item_id: params[:item_id])
     @order.save
+    binding.pry
+    ShippingAddress.create(shipping_address_params(@order))
     # binding.pry
     # @transaction = Transaction.new(order_params)
     # @transaction.save
@@ -18,5 +20,9 @@ class OrdersController < ApplicationController
 
   def order_params
     # params.require(:transaction).permit(:user_id, :item_id,:postal_code, :prefecture, :city, :block, :building, :telephone_number, :order_id)
+  end
+
+  def shipping_address_params(order)
+    params.permit(:postal_code, :prefecture_id, :city, :block, :building, :telephone_number).merge(order_id: @order.id)
   end
 end

254c1d3209b43725e54b1ade29e6c8344a3daa32
takumiiwasaki1101 <lcmwanol@gmail.com>
2020-11-07 15:05:24 +0900

    フォームオブジェクトの導入に伴い削除

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,15 +2,10 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
-    # @order = Order.new
     @transaction = Transaction.new
   end
 
   def create
-    # @order = Order.new(user_id: current_user.id, item_id: params[:item_id])
-    # @order.save
-    # ShippingAddress.create(shipping_address_params(@order))
-    # binding.pry
     @item = Item.find(params[:item_id])
     @transaction = Transaction.new(order_params)
       if @transaction.valid?
@@ -26,8 +21,4 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:transaction).permit(:postal_code, :prefecture_id, :city, :block, :building, :telephone_number, :order_id).merge(user_id: current_user.id, item_id: params[:item_id])
   end
-
-  # def shipping_address_params(order)
-  #   params.permit(:postal_code, :prefecture_id, :city, :block, :building, :telephone_number).merge(order_id: @order.id)
-  # end
 end
--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -4,19 +4,4 @@ class ShippingAddress < ApplicationRecord
 
   # Association
   belongs_to :order
-
-  # Validation
-  ## 入力必須に関するvalidation
-  with_options presence: true do
-    validates :postal_code
-    validates :prefecture_id
-    validates :city
-    validates :block
-    validates :telephone_number,numericality: { with: /\A[0-9]+\z/, message: 'Half-width number' }
-  end
-
-  ## id0以外での入力を必須とするvalidation
-  with_options numericality: { other_than: 0, message: 'Select' } do
-    validates :prefecture_id
-  end
 end

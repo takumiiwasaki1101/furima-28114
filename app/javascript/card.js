aeb21bf868e09577cef316c1dff974cd972a0de3
takumiiwasaki1101 <lcmwanol@gmail.com>
2020-11-10 11:11:42 +0900

    カード情報のトークン化

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,8 +1,25 @@
 const pay = () => {
+  Payjp.setPublicKey("pk_test_e10e4c24f08bb53ed1a5dff6")
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log("フォーム送信時にイベント発火")
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("transaction[number]"),
+      exp_month: formData.get("transaction[exp_month]"),
+      exp_year: `20${formData.get("transaction[exp_year]")}`,
+      cvc: formData.get("transaction[cvc]")
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        console.log(token)
+      }
+    });
   });
 };
 
